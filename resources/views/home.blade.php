da795617052746249fab172f9e550773cc4e6941
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-09-16 15:23:10 +0200

    feat: add file config

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,25 @@
 {{-- questa view estende il file main.blade.php che è dentro la cartella view/layouts --}}
 @extends('layouts.main')
 
-@section('hero')
-    <div>HERO HOME</div>
-@endsection
+
 
 @section('content')
-<div class="container my-5">
-    <h1>{{ $title }}</h1>
-    <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid">
-    <p>
-        {{ $text }}
-    </p>
-</div>
+    <div class="container my-5">
+        @foreach ($comics as $comic)
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $comic->title }}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        @endforeach
 
+
+    </div>
 @endsection
 
 
